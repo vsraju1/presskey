eb5fe7c149e9721af0ff86905ca9c0a1393e2c16
vsraju1 <69906555+vsraju1@users.noreply.github.com>
2023-09-16 13:10:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const keyPressed = document.querySelector("#insert")
+
+window.addEventListener("keydown", function (e) {
+    keyPressed.innerHTML =
+        `<table>
+            <tr>
+                <td>key</td>
+                <td>key code</td>
+                <td>code</td>
+            </tr>
+            <tr>
+                <td>${e.key === " " ? "space":e.key}</td>
+                <td>${e.keyCode}</td>
+                <td>${e.code}</td>
+            </tr>
+    </table>`;
+})
